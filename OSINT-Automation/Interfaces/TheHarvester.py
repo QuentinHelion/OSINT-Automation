bf56e4ba651a04e9b490f676263e0388ac293a13
QuentinHelion <helion.quentin@gmail.com>
2023-02-11 17:48:06 +0100

    add save result option

--- OSINT-Automation/Interfaces/TheHarvester.py
+++ OSINT-Automation/Interfaces/TheHarvester.py
@@ -1,11 +1,10 @@
 import os
 
 class TheHarvester:
-    def __init__(self, target, limit=500, source="yahoo"):
+    def __init__(self, target, source="yahoo"):
         self.target = target
-        self.limit = limit
         self.source = source
 
     def run(self):
-        # os.system("python3 lib/theHarvester/theHarvester.py -d {self.target} -l {self.limit} -b {self.source}")
-        os.system("python3 OSINT-Automation/lib/theHarvester/theHarvester.py -d google -l 500 -b yahoo")
+        command = "python3 OSINT-Automation/lib/theHarvester/theHarvester.py -d " + self.target + " -b " + self.source + " -f OSINT-Automation/results/theHarvester/" + self.target + ".txt"
+        os.system(command)
