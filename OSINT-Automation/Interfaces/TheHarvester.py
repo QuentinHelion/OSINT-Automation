a2db9376afa85021cdcbbc9c81faefb2bd4f3a67
QuentinHelion <helion.quentin@gmail.com>
2023-02-11 16:29:14 +0100

    functional docker

--- OSINT-Automation/Interfaces/TheHarvester.py
+++ OSINT-Automation/Interfaces/TheHarvester.py
@@ -1,10 +1,11 @@
 import os
 
 class TheHarvester:
-    def __init__(self, target, limit=500, source="google"):
+    def __init__(self, target, limit=500, source="yahoo"):
         self.target = target
         self.limit = limit
         self.source = source
 
     def run(self):
-        os.system("python3 lib/theHarvester/theHarvester.py -d {self.target} -l {self.limit} -b {self.source}")
+        # os.system("python3 lib/theHarvester/theHarvester.py -d {self.target} -l {self.limit} -b {self.source}")
+        os.system("python3 OSINT-Automation/lib/theHarvester/theHarvester.py -d google -l 500 -b yahoo")
